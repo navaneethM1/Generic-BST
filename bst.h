cb28d4723abf8f871cd013c05cf0402019f8ce09
Navaneeth M <53904189+navaneethM1@users.noreply.github.com>
2021-04-01 15:51:31 +0530

    First Commit

---
+++ bst.h
@@ -0,0 +1,253 @@
+#include <iostream>
+#include <functional>
+using namespace std;
+
+#define COUNT 10
+
+void disp(auto first, auto last)
+{
+	while(first != last)
+	{
+		cout << *first << "\t";
+		++first;
+	}
+	cout << "\n";
+}
+
+
+// Interface
+
+
+template<typename T>
+struct TreeNode
+{
+	T data;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode *parent;
+	TreeNode(T data) : data(data), left(nullptr), right(nullptr), parent(nullptr) { }
+};
+
+template<typename T, typename Compare = less<T>>
+class BST
+{
+	private:
+	TreeNode<T> *root;
+	TreeNode<T> *dummy;
+	Compare cmp; // will have operator() overloaded for comparision
+	int cnt;
+	void release(TreeNode<T> *root);
+	void printUtil(TreeNode<T> *root, int space);
+	static TreeNode<T> *min(TreeNode<T> *node);
+	static TreeNode<T> *max(TreeNode<T> *node);
+	static TreeNode<T> *inorder_successor(TreeNode<T> *node);
+	static TreeNode<T> *inorder_predecessor(TreeNode<T> *node);
+	public:
+	BST() : root(new TreeNode<T>(T())), dummy(nullptr), cmp(Compare()), cnt(0) { dummy = root; }
+	~BST() { release(root); }
+	BST(const BST&) = delete;
+	BST& operator=(const BST&) = delete;
+	class Iterator
+	{
+		private:
+		TreeNode<T> *p_it_;
+		public:
+		Iterator(TreeNode<T> *p_it) : p_it_(p_it) { }
+		Iterator& operator++() // pre
+		{
+			p_it_ = inorder_successor(p_it_);
+			return *this;
+		}
+		Iterator operator++(int) // post
+		{
+			Iterator temp(*this);
+			++*this;
+			return temp;
+		}
+		Iterator& operator--() // pre
+		{
+			p_it_ = inorder_predecessor(p_it_);
+			return *this;
+		}
+		Iterator operator--(int) // post
+		{
+			Iterator temp(*this);
+			--*this;
+			return temp;
+		}
+		T& operator*()
+		{
+			return p_it_->data;
+		}
+		bool operator==(const Iterator& rhs) const
+		{
+			return p_it_ == rhs.p_it_;
+		}
+		bool operator!=(const Iterator& rhs) const
+		{
+			return !(*this == rhs);
+		}
+	};
+	void insert(T x);
+	Iterator search(T x);
+	void remove(T x);
+	void preorder();
+	void inorder();
+	void postorder();
+	int totalcount() { return cnt; }
+	int leafcount();
+	int height();
+	void print();
+	Iterator min_element() { return Iterator(min(root)); }
+	Iterator max_element() { return cnt == 0 ? Iterator(dummy) : Iterator(max(root)->parent); }
+	Iterator begin() { return Iterator(min(root)); }
+	Iterator end() { return Iterator(dummy); }
+};
+
+
+// Implementation
+
+
+template<typename T, typename Compare>
+TreeNode<T> *BST<T, Compare>::min(TreeNode<T> *node)
+{
+	if(node == nullptr)
+		return node;
+	while(node->left)
+		node = node->left;
+	return node;
+}
+
+template<typename T, typename Compare>
+TreeNode<T> *BST<T, Compare>::max(TreeNode<T> *node)
+{
+	if(node == nullptr)
+		return node;
+	while(node->right)
+		node = node->right;
+	return node;
+}
+
+template<typename T, typename Compare>
+TreeNode<T> *BST<T, Compare>::inorder_successor(TreeNode<T> *node)
+{
+	if(node->right)
+		return min(node->right);
+	TreeNode<T> *p = node->parent;
+	while(p && node == p->right)
+	{
+		node = p;
+		p = p->parent;
+	}
+	return p;
+}
+
+template<typename T, typename Compare>
+TreeNode<T> *BST<T, Compare>::inorder_predecessor(TreeNode<T> *node)
+{
+	if(node->left)
+		return max(node->left);
+	TreeNode<T> *p = node->parent;
+	while(p && node == p->left)
+	{
+		node = p;
+		p = p->parent;
+	}
+	return p;
+}
+
+template<typename T, typename Compare>
+void BST<T, Compare>::insert(T x)
+{
+	if(root == dummy)
+	{
+		TreeNode<T> *temp = new TreeNode<T>(x);
+		temp->right = dummy;
+		dummy->parent = temp;
+		root = temp;
+		++cnt;
+	}
+	else
+	{
+		TreeNode<T> *prev = nullptr;
+		TreeNode<T> *curr = root;
+		while(curr && curr != dummy)
+		{
+			prev = curr;
+			bool lesser = cmp(x, curr->data);
+			if(!lesser && !cmp(curr->data, x))
+			{
+				return;
+			}
+			if(lesser)
+			{
+				curr = curr->left;
+			}
+			else
+			{
+				curr = curr->right;
+			}
+		}
+		++cnt;
+		TreeNode<T> *temp = new TreeNode<T>(x);
+		if(cmp(x, prev->data))
+		{
+			prev->left = temp;
+		}
+		else
+		{
+			prev->right = temp;
+			if(curr == dummy)
+			{
+				temp->right = dummy;
+				dummy->parent = temp;
+			}
+		}
+		temp->parent = prev;
+	}
+}
+
+template<typename T, typename Compare>
+void BST<T, Compare>::release(TreeNode<T> *root)
+{
+	if(root)
+	{
+		release(root->left);
+		release(root->right);
+		delete root;
+	}
+	return;
+}
+
+template<typename T, typename Compare>
+void BST<T, Compare>::printUtil(TreeNode<T> *root, int space) 
+{ 
+    // Base case 
+    if (root == nullptr || root == dummy) 
+        return; 
+  
+    // Increase distance between levels 
+    space += COUNT; 
+  
+    // Process right child first 
+    printUtil(root->right, space); 
+  
+    // Print current node after space 
+    // count 
+    printf("\n"); 
+    for (int i = COUNT; i < space; i++) 
+        cout<<" "; 
+    cout<<root->data<<"\n"; 
+  
+    // Process left child 
+    printUtil(root->left, space); 
+}
+ 
+template<typename T, typename Compare>
+// Wrapper over printUtil()
+void BST<T, Compare>::print() 
+{ 
+    // Pass initial space count as 0 
+    printUtil(root, 0); 
+    printf("\n");
+}
---
+++ client.cpp
@@ -0,0 +1,89 @@
+#include <algorithm>
+#include "bst.h"
+
+#if 0
+struct MyStringCompare
+{
+	bool operator()(string s1, string s2)
+	{ return s1.length() < s2.length(); }
+};
+#endif
+
+int main()
+{
+
+#if 0
+	BST<string> bst;
+	// BST<string, MyStringCompare> bst;
+	bst.insert("rama");
+	bst.insert("ashok");
+	bst.insert("zebra");
+	bst.insert("rama");
+	bst.insert("zz");	
+	bst.insert("aa");
+
+	bst.print();
+	cout << bst.totalcount() << "\n\n";
+
+	cout << *min_element(bst.begin(), bst.end()) << "\n";
+	cout << *max_element(bst.begin(), bst.end()) << "\n";
+	cout << *bst.min_element() << "\n";
+	cout << *bst.max_element() << "\n";
+	cout << "\n";
+
+	cout << "Inorder\n";
+	disp(bst.begin(), bst.end());
+	cout << "\n";
+
+	cout << "Reverse Order\n";
+	auto it = bst.end();
+	--it;
+	while(it != bst.begin())
+	{
+		cout << *it << "\t";
+		--it;
+	}
+	cout << *it;
+	cout << "\n\n";
+
+	return 0;
+#endif
+
+#if 0
+	BST<int> bst;
+	bst.insert(50);
+	bst.insert(30);
+	bst.insert(20);
+	bst.insert(40);
+	bst.insert(70);	
+	bst.insert(60);	
+	bst.insert(80);
+
+	bst.print();
+	cout << bst.totalcount() << "\n\n";
+
+	cout << *min_element(bst.begin(), bst.end()) << "\n";
+	cout << *max_element(bst.begin(), bst.end()) << "\n";
+	cout << *bst.min_element() << "\n";
+	cout << *bst.max_element() << "\n";
+	cout << "\n";
+
+	cout << "Inorder\n";
+	disp(bst.begin(), bst.end());
+	cout << "\n";
+
+	cout << "Reverse Order\n";
+	auto it = bst.end();
+	--it;
+	while(it != bst.begin())
+	{
+		cout << *it << "\t";
+		--it;
+	}
+	cout << *it;
+	cout << "\n\n";
+
+	return 0;
+#endif
+
+}
