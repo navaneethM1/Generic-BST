77c625695a9cdb90a2e856170aabbee3a042ff0e
navaneethM1 <navaneethmukund27@gmail.com>
2021-04-23 13:21:00 +0530

    define date class friend functions outside

--- include/date.h
+++ include/date.h
@@ -11,20 +11,24 @@ class Date
 		int yy_;
 	public:
 		Date(int dd = 1, int mm = 1, int yy = 1970) : dd_(dd), mm_(mm), yy_(yy) { }
-		friend ostream& operator<<(ostream& o, const Date& d)
-		{
-			return o << d.dd_ << "-" << d.mm_ << "-" << d.yy_;
-		}
-		friend bool operator<(const Date& lhs, const Date& rhs)
-		{
-			if(lhs.yy_ < rhs.yy_)
-				return true;
-			if(lhs.yy_ == rhs.yy_ && lhs.mm_ < rhs.mm_)
-				return true;
-			if(lhs.yy_ == rhs.yy_ && lhs.mm_ == rhs.mm_ && lhs.dd_ < rhs.dd_) 
-				return true;
-			return false;
-		}
+		friend ostream& operator<<(ostream& o, const Date& d);
+		friend bool operator<(const Date& lhs, const Date& rhs);
 };
+
+ostream& operator<<(ostream& o, const Date& d)
+{
+	return o << d.dd_ << "-" << d.mm_ << "-" << d.yy_;
+}
+
+bool operator<(const Date& lhs, const Date& rhs)
+{
+	if(lhs.yy_ < rhs.yy_)
+		return true;
+	if(lhs.yy_ == rhs.yy_ && lhs.mm_ < rhs.mm_)
+		return true;
+	if(lhs.yy_ == rhs.yy_ && lhs.mm_ == rhs.mm_ && lhs.dd_ < rhs.dd_) 
+		return true;
+	return false;
+}
 
 #endif
