d842788ceda6ac1327890f1f92717fbf3ef49a48
mohantyabhijit074 <mohantyabhijit074@gmail.com>
2021-05-04 19:59:14 +0530

    made changes in client file

--- client.cpp
+++ client.cpp
@@ -3,157 +3,15 @@
 #include "./include/bst.h"
 #include "./include/date.h"
 
-#if 0
 struct MyStringCompare
 {
 	bool operator()(string s1, string s2)
 	{ return s1.length() < s2.length(); }
 };
-#endif
 
 int main()
 {
-
-#if 0
-	BST<string> bst;
-	// BST<string, MyStringCompare> bst;
-	bst.insert("rama");
-	bst.insert("ashok");
-	bst.insert("zebra");
-	bst.insert("rama");
-	bst.insert("zz");
-	bst.insert("aa");
-
-	bst.print();
-	cout << bst.totalcount() << "\n\n";
-
-	cout << *min_element(bst.begin(), bst.end()) << "\n";
-	cout << *max_element(bst.begin(), bst.end()) << "\n";
-	cout << *bst.min_element() << "\n";
-	cout << *bst.max_element() << "\n";
-	cout << "\n";
-
-	cout << "Inorder\n";
-	disp(bst.begin(), bst.end());
-	cout << "\n";
-
-	cout << "Reverse Order\n";
-	auto it = bst.end();
-	--it;
-	while(it != bst.begin())
-	{
-		cout << *it << "\t";
-		--it;
-	}
-	cout << *it;
-	cout << "\n\n";
-
-	auto itf = find(bst.begin(), bst.end(), "zebra");
-	if(itf != bst.end())
-	{
-		cout << *itf << " found\n\n";
-	}
-	else
-	{
-		cout << "not found\n\n";
-	}
-
-	cout << count(bst.begin(), bst.end(), "nothing") << "\n\n";
-
-	itf = bst.search("ashok");
-	if(itf != bst.end())
-	{
-		cout << *itf << " found\n\n";
-	}
-	else
-	{
-		cout << "not found\n\n";
-	}
-
-	bst.preorder();
-	bst.inorder();
-	bst.postorder();
-	cout << "\n";
-
-	cout << "Leafcount: " << bst.leafcount() << "\n";
-	cout << "Height: " << bst.height() << "\n\n";
-
-	return 0;
-#endif
-
-#if 0
-	// BST<string> bst;
-	// BST<string, MyStringCompare> bst;
-	vector<string> v = { "rama", "ashok", "zebra", "rama", "zz", "aa" };
-	// bst.insert("rama");
-	// bst.insert("ashok");
-	// bst.insert("zebra");
-	// bst.insert("rama");
-	// bst.insert("zz");
-	// bst.insert("aa");
-	BST<string> bst1(v.begin(), v.end());
-	BST<string> bst;
-	cout << "Copy assignment\n";
-	bst = bst1;
-	cout << "\n";
-	bst.print();
-	cout << bst.totalcount() << "\n\n";
-
-	cout << *min_element(bst.begin(), bst.end()) << "\n";
-	cout << *max_element(bst.begin(), bst.end()) << "\n";
-	cout << *bst.min_element() << "\n";
-	cout << *bst.max_element() << "\n";
-	cout << "\n";
-
-	cout << "Inorder\n";
-	disp(bst.begin(), bst.end());
-	cout << "\n";
-
-	cout << "Reverse Order\n";
-	auto it = bst.end();
-	--it;
-	while(it != bst.begin())
-	{
-		cout << *it << "\t";
-		--it;
-	}
-	cout << *it;
-	cout << "\n\n";
-
-	auto itf = find(bst.begin(), bst.end(), "zebra");
-	if(itf != bst.end())
-	{
-		cout << *itf << " found\n\n";
-	}
-	else
-	{
-		cout << "not found\n\n";
-	}
-
-	cout << count(bst.begin(), bst.end(), "nothing") << "\n\n";
-
-	itf = bst.search("ashok");
-	if(itf != bst.end())
-	{
-		cout << *itf << " found\n\n";
-	}
-	else
-	{
-		cout << "not found\n\n";
-	}
-
-	bst.preorder();
-	bst.inorder();
-	bst.postorder();
-	cout << "\n";
-
-	cout << "Leafcount: " << bst.leafcount() << "\n";
-	cout << "Height: " << bst.height() << "\n\n";
-
-	return 0;
-#endif
-
-#if 0
+	#if 0
 	BST<int> bst;
 	bst.insert(50);
 	bst.insert(30);
@@ -165,9 +23,10 @@ int main()
 
 	bst.print();
 	cout << bst.totalcount() << "\n\n";
-
+	//STL's
 	cout << *min_element(bst.begin(), bst.end()) << "\n";
 	cout << *max_element(bst.begin(), bst.end()) << "\n";
+	// Implemented
 	cout << *bst.min_element() << "\n";
 	cout << *bst.max_element() << "\n";
 	cout << "\n";
@@ -186,7 +45,7 @@ int main()
 	}
 	cout << *it;
 	cout << "\n\n";
-
+	// STL's search
 	auto itf = find(bst.begin(), bst.end(), 90);
 	if(itf != bst.end())
 	{
@@ -194,7 +53,7 @@ int main()
 	}
 	else
 	{
-		cout << "not found\n\n";
+		cout << "90" <<" not found\n\n";
 	}
 
 	cout << count(bst.begin(), bst.end(), 40) << "\n\n";
@@ -206,7 +65,7 @@ int main()
 	}
 	else
 	{
-		cout << "not found\n\n";
+		cout << 150 <<" not found\n\n";
 	}
 
 	bst.preorder();
@@ -217,67 +76,59 @@ int main()
 	cout << "Leafcount: " << bst.leafcount() << "\n";
 	cout << "Height: " << bst.height() << "\n\n";
 
-	return 0;
-#endif
-
-#if 0
-	BST<Date> bst;
-	bst.insert({11,11,2021});
-	bst.insert({12,9,2021});
-	bst.insert({1,1,2019});
-	bst.insert({2,10,2020});
+	//remove node having both subtree
+	bst.remove(70);
+	cout << "After removing 70" <<"\n";
 	bst.print();
-	cout << *min_element(bst.begin(), bst.end()) << "\n";
-	cout << *max_element(bst.begin(), bst.end()) << "\n";
-	cout << *bst.min_element() << "\n";
-	cout << *bst.max_element() << "\n";
 
-	auto itf = bst.search({11,11,2021});
-	if(itf != bst.end())
-	{
-		cout << *itf << " found\n\n";
-	}
-	else
-	{
-		cout << "not found\n\n";
-	}
-	cout << "Reverse Order\n";
-	auto it = bst.end();
-	--it;
-	while(it != bst.begin())
-	{
-		cout << *it << "\t";
-		--it;
-	}
-	cout << *it;
-	cout << "\n\n";
+	bst.remove(50);
+	cout << "After removing 50" <<"\n";
+	bst.print();
+
+	bst.remove(20);
+	bst.remove(40);
+	cout << "After removing 20 and 40" <<"\n";
+	bst.print();
+	cout << "Leafcount: " << bst.leafcount() << "\n";
+	cout << "Height: " << bst.height() << "\n\n";
+
+	BST<int>::Iterator::value_type temp;
+	temp = 10;
+
+	#endif	
+
+	#if 0
+	//String with Explicit binary predicate(Comparator)
+	BST<string, MyStringCompare> bst;
+	bst.insert("rama");
+	bst.insert("ashok");
+	bst.insert("zebra");
+	bst.insert("rama");
+	bst.insert("zz");
+	bst.insert("aa");
+	bst.print();
+
+	cout <<"After Copy constructor\n";
+	BST<string,MyStringCompare> bst_copy(bst);
+	bst_copy.print();
+	#endif 
+	
+	#if 0
+	//User Defined Data type : Date type 
+	BST<Date> bst_date;
+	bst_date.insert({11,11,2021});
+	bst_date.insert({12,9,2021});
+	bst_date.insert({1,1,2019});
+	bst_date.insert({2,10,2020});
+	bst_date.insert({21,11,2022});
+	bst_date.insert({10,12,2021});
+	bst_date.print();
+
+	cout << "After Copy Assignment operator\n";
+	BST<Date> bst_date_assignment;
+	bst_date_assignment = bst_date;
+	bst_date_assignment.print();
+	#endif 
 
 	return 0;
-#endif
-
-#if 1
-	vector<int> a{10, 1, 3,20, 15};
-	vector<int> b{12,4,2,54,121,50,52};
-	BST<int> rhs(begin(a), end(a));
-	BST<int> lhs(begin(b), end(b));
-	// cout << rhs.height();
-	cout << "First BST\n";
-	rhs.print();
-	cout << "Second BST\n";
-	lhs.print();
-	cout << "\nCopy Assignment\n";
-	lhs = rhs;
-	lhs.print();
-
-	// cout << "\nCount " << lhs.totalcount() << "\n";
-#endif
-
-#if 0
-	vector<int> b{12,4,2,54,121};
-	BST<int> rhs(begin(b), end(b));
-	cout << "rhs\n";
-	rhs.print();
-	BST<int> lhs(rhs);
-	lhs.print();
-#endif
 }
